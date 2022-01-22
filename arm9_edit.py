5a25dc45f24e327e4c2fdd7c4beb2a14629845b6
Firas S <choogiesaur@users.noreply.github.com>
2022-01-22 11:07:10 -0500

    Readability, comments

--- arm9_edit.py
+++ arm9_edit.py
@@ -14,17 +14,17 @@ print(len(rom.arm9))
 
 # Keep track of raw arm9 bytes and decompress with ndspy
 raw_arm9 = rom.arm9
-decomp_arm9 = comp.decompress(raw_arm9)
+decompressed_arm9 = comp.decompress(raw_arm9)
 print('\nDecompressed arm9 byte size:')
-print(len(decomp_arm9))
+print(len(decompressed_arm9))
 
 # Modify byte at offset 0x70080; default is 0x8
 # Check readme for offsets for other versions
 shiny_value = 0xFF
 
-print("\nValue before: ", decomp_arm9[0x70080])
-decomp_arm9[0x70080] = shiny_value
-print("Value after: ", decomp_arm9[0x70080])
+print("\nValue before: ", decompressed_arm9[0x70080])
+decompressed_arm9[0x70080] = shiny_value
+print("Value after: ", decompressed_arm9[0x70080])
 
 # Dont recompress - desmume crash
 # recomp_arm9 = comp.compress(decomp_arm9)
@@ -32,5 +32,5 @@ print("Value after: ", decomp_arm9[0x70080])
 # print(len(recomp_arm9))
 
 # rom.arm9 = recomp_arm9
-rom.arm9 = decomp_arm9
+rom.arm9 = decompressed_arm9
 rom.saveToFile('soulsilver_edit.nds')
