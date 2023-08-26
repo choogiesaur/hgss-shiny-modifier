baca4a3a61bea00cd4cf863ddb511b5dcbe6f77e
Firas S <choogiesaur@users.noreply.github.com>
2023-08-26 12:22:58 -0400

    Add comment regarding not recompressing arm9

--- shiny_rate_editor.py
+++ shiny_rate_editor.py
@@ -32,6 +32,7 @@ def modify_shiny_rate(filename, shiny_offset='AUTO', new_shiny_rate=0xFF):
     decompressed_arm9[shiny_offset] = new_shiny_rate
     print(f"Shiny Value at offset {shiny_offset} after: {decompressed_arm9[shiny_offset]}")
 
+    # Note that we do not recompress to avoid crashes.
     rom.arm9 = decompressed_arm9
     rom.saveToFile('shiny_rate_patched.nds')
 
