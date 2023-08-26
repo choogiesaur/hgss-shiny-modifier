b7bbc981f1e44d64585b1698b3a301c5fc7cbbc5
Firas S <choogiesaur@users.noreply.github.com>
2023-08-26 12:20:14 -0400

    Cleanup and use dict instead of if/else cases

--- shiny_rate_editor.py
+++ shiny_rate_editor.py
@@ -4,57 +4,44 @@ import sys
 
 ## Change shiny rate in Pokemon HeartGold/SoulSilver! ##
 # Place your .nds file in the same folder as this script.
-# Modify 'shiny_offset' and 'new_shiny_rate' below.
 # Usage: python shiny_rate_editor.py <filename> [<shiny_offset>] [<new_shiny_rate>]
 
 def modify_shiny_rate(filename, shiny_offset='AUTO', new_shiny_rate=0xFF):
-
     # Load rom with ndspy and print rom information
     rom = ndspy.rom.NintendoDSRom.fromFile(filename)
-    print('Rom Name:', rom.name.decode(), ' Rom idCode: ', rom.idCode.decode())
-    print('\nRaw arm9 byte size:', len(rom.arm9))
+    print(f'Rom Name: {rom.name.decode()} Rom idCode: {rom.idCode.decode()}')
+    print(f'\nRaw arm9 byte size: {len(rom.arm9)}')
 
-    # Auto-detect HGSS version and use appropriate shiny offset if custom offset not supplied
-    title_id = rom.idCode.decode()
-    if shiny_offset == 'AUTO':
-        # Japanese HG or SS
-        if title_id == 'IPKJ' or title_id =='IPGJ':
-            shiny_offset = 0x6FAC0
-        # Spanish HG
-        elif title_id == 'IPKS':
-            shiny_offset = 0x70078
-        # Korean HG
-        elif title_id == 'IPKK':
-            shiny_offset = 0x7017C
-        # Korean SS
-        elif title_id == 'IPGK':
-            shiny_offset = 0x70174
-        # Other regions (USA included)...
-        else:
-            shiny_offset = 0x70080
+    # Map for title IDs and their shiny offsets
+    shiny_offsets = {
+        'IPKJ': 0x6FAC0, 'IPGJ': 0x6FAC0,  # Japanese HG or SS
+        'IPKS': 0x70078,  # Spanish HG
+        'IPKK': 0x7017C,  # Korean HG
+        'IPGK': 0x70174   # Korean SS
+    }
 
-    # Japanese IPKJ (HG) IPGJ (SS)
-    # Spanish HG - IPKS
-    # Korean HG - IPKK
-    # Korean SS - IPGK
-    # Others...
+    # Use appropriate shiny offset if custom offset not supplied
+    # 0x70080 is the default, used in the US version and a few others
+    shiny_offset = shiny_offsets.get(rom.idCode.decode(), 0x70080) if shiny_offset == 'AUTO' else shiny_offset
 
-    # Keep track of raw arm9 bytes and decompress with ndspy
+    # Decompress arm9 with ndspy
     decompressed_arm9 = comp.decompress(rom.arm9)
-    print('\nDecompressed arm9 byte size:', len(decompressed_arm9))
+    print(f'\nDecompressed arm9 byte size: {len(decompressed_arm9)}')
 
-    print("\nShiny Value at offset ", shiny_offset, " before:", decompressed_arm9[shiny_offset])
+    print(f"\nShiny Value at offset {shiny_offset} before: {decompressed_arm9[shiny_offset]}")
     decompressed_arm9[shiny_offset] = new_shiny_rate
-    print("Shiny Value at offset ", shiny_offset, " after:", decompressed_arm9[shiny_offset])
+    print(f"Shiny Value at offset {shiny_offset} after: {decompressed_arm9[shiny_offset]}")
 
     rom.arm9 = decompressed_arm9
     rom.saveToFile('shiny_rate_patched.nds')
 
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2 or len(sys.argv) > 4:
-        print("Usage: python shiny_editor.py <filename> [<shiny_offset>] [<new_shiny_rate>]")
-    else:
+    argc = len(sys.argv)
+    if 2 <= argc <= 4:
         filename = sys.argv[1]
-        shiny_offset = int(sys.argv[2], 16) if len(sys.argv) >= 3 else 0x70080
-        new_shiny_rate = int(sys.argv[3], 16) if len(sys.argv) >= 4 else 0xFF
+        shiny_offset = int(sys.argv[2], 16) if argc >= 3 else 'AUTO'
+        new_shiny_rate = int(sys.argv[3], 16) if argc >= 4 else 0xFF
         modify_shiny_rate(filename, shiny_offset, new_shiny_rate)
+    else:
+        print("Usage: python shiny_editor.py <filename> [<shiny_offset>] [<new_shiny_rate>]")
