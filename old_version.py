431bafddbb09232dfd50a6aa36d61a482202fae1
Firas S. <choogiesaur@users.noreply.github.com>
2025-08-20 15:45:08 -0400

    clarify and clean up

--- old_version.py
+++ old_version.py
@@ -5,7 +5,8 @@ import sys
 ## Change shiny rate in Pokemon HeartGold/SoulSilver! ##
 # Place your .nds file in the same folder as this script.
 # Modify 'shiny_offset' and 'new_shiny_rate' below.
-# Usage: python shiny_editor.py <filename>
+# Optional: modify output file name
+# Usage: python old_version.py <filename>
 
 def modify_shiny_rate(filename):
 
@@ -19,11 +20,6 @@ def modify_shiny_rate(filename):
 	decompressed_arm9 = comp.decompress(raw_arm9)
 	print('\nDecompressed arm9 byte size:', len(decompressed_arm9))
 
-	# Set the appropriate shiny rate offset here.
-	# For most versions of HGSS it is located at 0x70080;
-	# If yours is one of the regions below, change 'shiny_offset' accordingly.
-	shiny_offset = 0x70080
-	
 	# Region      Shiny rate offset
 	# -----------------------------
 	# Japanese    0×6FAC0
@@ -31,22 +27,31 @@ def modify_shiny_rate(filename):
 	# Korean HG   0×7017C
 	# Korean SS   0×70174
 	# Others      0×70080
+	
+	# Set the appropriate shiny rate offset here from the table shown above.
+	# For most versions of HGSS it is located at 0x70080
+	shiny_offset = 0x70080
 
-	# Set the desired shiny rate. 0x8 or 1/8192 is the in-game default.
+	# Set the desired shiny rate. 0x08 or 1/8192 is the in-game default.
 	# 0xFF or 1/256 is this script's default, and the highest that can be set.
 	# You can select a value from 0x08 to 0xFF.
 	new_shiny_rate = 0xFF
 
+	# Modify the value here to change your output file name
+	output_file_name = 'shiny_rate_patched.nds'
+
+	# Just debug info to verify before and after values
 	print("\nValue before: ", decompressed_arm9[shiny_offset])
 	decompressed_arm9[shiny_offset] = new_shiny_rate
 	print("Value after: ", decompressed_arm9[shiny_offset])
 
 	rom.arm9 = decompressed_arm9
-	rom.saveToFile('shiny_rate_patched.nds')
+	rom.saveToFile(output_file_name)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python shiny_editor.py <filename>")
+        print("Usage: ")
+		print("python old_version.py <filename>")
     else:
         filename = sys.argv[1]
         modify_shiny_rate(filename)
